4e2f493fb11e52b114a4fee2ffd945bae2ee465b
orvice <orvice@gmail.com>
2015-09-08 22:26:23 +0800

    add func

--- src/NodeQuery/Client.php
+++ src/NodeQuery/Client.php
@@ -13,8 +13,23 @@ class Client{
         $this->key = $key;
     }
 
-    public function serverList(){
+    public function request($url)
+    {
+        $ch = curl_init();
+        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($ch, CURLOPT_URL,$url);
+        $resp = curl_exec($ch);
+        curl_close($ch);
+        return $resp;
+    }
+
+    public function serverList()
+    {
         $url = $this->serverListUrl.$this->key;
+        $rt = $this->request($url);
+        $srvAry = json_decode($rt,true);
+        return $srvAry;
     }
 
 }
