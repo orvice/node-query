9d6f9d35dd7c2329df8099309f208bf281eaab89
orvice <orvice@gmail.com>
2015-09-09 21:54:44 +0800

    update server

--- src/NodeQuery/Server.php
+++ src/NodeQuery/Server.php
@@ -6,7 +6,18 @@ namespace NodeQuery;
 
 class Server
 {
-    public function __construct($key,$id){
+    private $baseUrl = "https://nodequery.com/api/servers/";
 
+    private $key, $id;
+
+    public function __construct($key,$id){
+        $this->key = $key;
+        $this->id =$id;
+    }
+
+    public function serverAry(){
+        $url = $this->baseUrl . $this->id . "?api_key=" . $this->key;
+        $resp = Request::get($url);
+        return json_decode($resp,true);
     }
 }
