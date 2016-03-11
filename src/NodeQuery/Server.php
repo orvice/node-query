1b4abc6cced8a593a0a23f16f752ee7baf906b49
orvice <orvice@gmail.com>
2016-03-12 01:25:03 +0800

    update server

--- src/NodeQuery/Server.php
+++ src/NodeQuery/Server.php
@@ -15,28 +15,17 @@ class Server
         $this->id =$id;
     }
 
-    public function serverAry(){
+    public function infoAry(){
         $url = $this->baseUrl . $this->id . "?api_key=" . $this->key;
         $resp = Request::get($url);
         return json_decode($resp,true);
     }
-}
 
-class Loads
-{
-    private $baseUrl = "https://nodequery.com/api/loads/";
-
-    private $key, $id, $interval;
-
-    public function __construct($key,$id,$interval){
-        $this->key = $key;
-        $this->id = $id;
-        $this->interval = $interval
-    }
-
-    public function loadAry(){
-        $url = $this->baseUrl . $this->interval . "/" . $this->id . "?api_key=" . $this->key;
+    public function loadAry($interval){
+        $loadUrl = "https://nodequery.com/api/loads/";
+        $url = $loadUrl . $interval . "/" . $this->id . "?api_key=" . $this->key;
         $resp = Request::get($url);
         return json_decode($resp,true);
     }
+
 }
